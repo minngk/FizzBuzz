087bb013cc078fe6bcf8681e2b568d0a023e51e7
MiraiNiki <mirainiki@COINSs-iMac.local>
2014-04-24 22:13:34 +0900

    first commit

--- FizzBuzz/main.m
+++ FizzBuzz/main.m
@@ -13,9 +13,45 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        int b = 0;
+        //        for (b=0;b<1000;b++) {
+        //
+        //            if(b%3!=0 && b%5!=0 && b%15!=0){
+        //                NSLog(@"b=%d" ,b);
+        //            }
+        //            if(b>=100){
+        //                break;
+        //            }
+        //            if(b%3==0 && b%15!=0){
+        //                NSLog(@"yama");
+        //            }
+        //            if(b%5==0 && b%15!=0){
+        //                NSLog(@"haru");
+        //            }
+        //            if(b%15==0){
+        //                NSLog(@"yamaharu");
+        //            }
+        //
+        //        }
+        for (b=0;b<1000;b++) {
+            
+            if(b>=100){
+                break;
+            }
+            else if(b%15==0){
+                NSLog(@"yamaharu");
+            }
+            else if(b%3==0){
+                NSLog(@"yama");
+            }
+            else if(b%5==0){
+                NSLog(@"haru");
+            }
+            else{
+                NSLog(@"%d",b);
+            }
+            
+        }
     }
     return 0;
 }
