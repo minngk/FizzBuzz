5e734265f13d262eb2badc1a5add1398e2dffbb6
Mirai NIKI <mirainiki@COINSs-iMac.local>
2014-04-24 21:59:54 +0900

    Initial Commit

---
+++ FizzBuzz/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  FizzBuzz
+//
+//  Created by Mirai NIKI on 4/24/14.
+//  Copyright (c) 2014 Mirai NIKI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
